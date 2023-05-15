0441efd9cb94da9c71b08c7437f490b38412fc28
juri <jurgen.lubonja@eng.it>
2023-05-15 17:39:56 +0200

    installing angular material + adding primeng Stylization

--- Experimetn16/src/app/app.component.ts
+++ Experimetn16/src/app/app.component.ts
@@ -1,29 +1,39 @@
-import { Component, computed, effect, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AnotherOneComponent } from './another-one/another-one.component';
+import {Component, computed, effect, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AnotherOneComponent} from './another-one/another-one.component';
 import {RouterLink, RouterOutlet} from "@angular/router";
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
+import {OrderListModule} from "primeng/orderlist";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, AnotherOneComponent, RouterOutlet, RouterLink],
+  imports: [CommonModule,
+    AnotherOneComponent,
+    RouterOutlet,
+    RouterLink,
+    CardModule,
+    ButtonModule,
+    OrderListModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title:string = 'Testtest'
+  title: string = 'Testtest'
   firstName = signal('Jane');
   lastName = signal('Doe');
 
-  ArrayOfNames = signal(['Adam','Bravo','Charlie'])
+  ArrayOfNames = signal(['Adam', 'Bravo', 'Charlie'])
 
-  elArray = computed(()=> this.ArrayOfNames())
+  elArray = computed(() => this.ArrayOfNames())
   fullName = computed(() => `${this.firstName()} ${this.lastName()}`);
 
   constructor() {
     effect(() => console.log('Name changed:', this.fullName()));
     effect(() => console.log('Name changed:', this.fullName()));
-    effect(()=>console.log('Element changed:', this.elArray()))
+    effect(() => console.log('Element changed:', this.elArray()))
     console.log(this.elArray())
   }
 
@@ -31,15 +41,15 @@ export class AppComponent {
     this.firstName.set(newName);
   }
 
-  setLastName(newLast:string){
-    this.lastName.update(value => value.replace(value,newLast))
+  setLastName(newLast: string) {
+    this.lastName.update(value => value.replace(value, newLast))
   }
 
-  delEl(index:number){
+  delEl(index: number) {
     this.ArrayOfNames.update(value => {
       const arr = value
 
-     arr.splice(index,1)
+      arr.splice(index, 1)
       return arr
     })
   }
