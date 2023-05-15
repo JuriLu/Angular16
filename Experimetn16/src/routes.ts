8fa314ba11149ff523c7902815d4699328620979
juri <jurgen.lubonja@eng.it>
2023-05-15 17:00:54 +0200

    Installing prime ng

---
+++ Experimetn16/src/routes.ts
@@ -0,0 +1,6 @@
+import {Route} from "@angular/router";
+import {AboutComponent} from "./app/about/about.component";
+
+export const routes: Route[] = [
+  {path: 'about',component:AboutComponent}
+]
