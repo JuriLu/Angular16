3161b102b229ccce55565782cd9e26af6e60cb9b
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2023-05-15 12:14:34 +0000

    Angular 16 Initial Project + Signals

---
+++ Experimetn16/src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, computed, effect, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+  <h1>Hello</h1>
+  {{ fullName() }} 
+  <button (click)="setName('John')">Click First</button>
+  <button (click)="setLastName('Walker')">Click Last</button>
+  <ul >
+    <li>test</li>
+    <li 
+    *ngFor="let el of elArray();let i = index">{{el}}
+    <button (click)="delEl(i)">Del Element {{i}}</button>
+  </li>
+  </ul>
+`,  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  firstName = signal('Jane');
+  lastName = signal('Doe');
+
+  ArrayOfNames = signal(['Adam','Bravo','Charlie'])
+
+  elArray = computed(()=> this.ArrayOfNames())
+  fullName = computed(() => `${this.firstName()} ${this.lastName()}`);
+
+  constructor() {
+    effect(() => console.log('Name changed:', this.fullName()));
+    effect(() => console.log('Name changed:', this.fullName()));
+    effect(()=>console.log('Element changed:', this.elArray()))
+    console.log(this.elArray())
+  }
+
+  setName(newName: string) {
+    this.firstName.set(newName);
+  }
+
+  setLastName(newLast:string){
+    this.lastName.update(value => value.replace(value,newLast))
+  }
+
+  delEl(index:number){
+    this.ArrayOfNames.update(value => {
+      value.slice(index)
+      return value
+    })
+  }
+  
+
+}
